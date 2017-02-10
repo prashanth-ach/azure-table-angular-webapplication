20ca1dd3383a5ba4d00bb7dbafa35764895f7c26
prashanth-ach <prashanth.ach@gmail.com>
2017-02-10 11:15:35 -0600

    added files

---
+++ WebApplication2/WebApplication2/Controllers/AzureTableController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AzureTableController : Controller
+    {
+        // GET: AzureTable
+        public ActionResult Index()
+        {
+            UsersList();
+            return View();
+        }
+        [HttpPost]
+    public ActionResult Index(String Id,string Name)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=magnetto;AccountKey=ZPpQmguO9Ds7xoXjjwvsmU7k0VDRPB24u4EjaK8Eo6fg0xaqrSqTiyvj4k1YbpjQsOXFrZsXQeevtGbR6L1Yqg==;");
+
+            CloudTableClient client = account.CreateCloudTableClient();
+
+            CloudTable table = client.GetTableReference("prash");
+
+            table.CreateIfNotExists();
+            TableOperation operation = TableOperation.Insert(new UserEntity().CreateUser( Id , Name));
+
+            table.Execute(operation);
+
+            UsersList();
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public ActionResult Delete(String RowKey, string PartitionKey)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=magnetto;AccountKey=ZPpQmguO9Ds7xoXjjwvsmU7k0VDRPB24u4EjaK8Eo6fg0xaqrSqTiyvj4k1YbpjQsOXFrZsXQeevtGbR6L1Yqg==;");
+
+            CloudTableClient client = account.CreateCloudTableClient();
+
+            CloudTable table = client.GetTableReference("prash");
+
+            TableOperation operation = TableOperation.Retrieve<UserEntity>(PartitionKey, RowKey);
+
+            TableResult isUser = table.Execute(operation);
+
+            UserEntity user = isUser.Result as UserEntity;
+
+            if(user!=null)
+            {
+                TableOperation deleteoperation = TableOperation.Delete(user);
+
+                table.Execute(deleteoperation);
+            }
+           
+            return RedirectToAction("Index");
+        }
+
+
+        public void UsersList()
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=magnetto;AccountKey=ZPpQmguO9Ds7xoXjjwvsmU7k0VDRPB24u4EjaK8Eo6fg0xaqrSqTiyvj4k1YbpjQsOXFrZsXQeevtGbR6L1Yqg==;");
+
+            CloudTableClient client = account.CreateCloudTableClient();
+
+            CloudTable table = client.GetTableReference("prash");
+
+            TableQuery<UserEntity> query = new TableQuery<UserEntity>();
+
+            ViewBag.userList = table.ExecuteQuery(query);
+
+
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/UserEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+namespace WebApplication2.Models
+{
+    public class UserEntity:TableEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public UserEntity()
+        {
+
+        }
+
+        public UserEntity CreateUser(string Id,string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.RowKey = Guid.NewGuid().ToString();
+            this.PartitionKey = DateTime.Now.Year.ToString();
+
+            return this;
+        }
+
+        internal ITableEntity CreateUser(object id, object name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
